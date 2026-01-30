88aa7b1566cd790a75f3b5f77955ddb9bd7ca790
rere005 <ramahomsi36@gmail.com>
2026-01-30 16:20:18 +0100

    .

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/api/health", (req, res) =>{
+    res.json({status: "ok"}); 
+});
+
+
+app.listen(5000, () => {
+    console.log("Backend kör på http://localhost:5000");
+
+});
