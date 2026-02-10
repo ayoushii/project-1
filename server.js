1501786b106a80f966ad1fa3aec1af91db319e68
rere005 <ramahomsi36@gmail.com>
2026-02-10 03:14:40 +0100

    MM

--- server.js
+++ server.js
@@ -165,6 +165,8 @@ app.post("/login", (req, res) => {
 });
 
 // start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Server running on port, PORT');
 });
+ 
