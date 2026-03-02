021646430fa1bdb5dab0b4d7808488c7eb510db6
rere005 <ramahomsi36@gmail.com>
2026-03-02 23:54:50 +0100

    Disable DB for Azure test

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const dbConfig = {
 // DB connect
 const db = mysql.createConnection(dbConfig);
 
-db.connect((err) => {
-  if (err) {
-    console.log("DB ERROR:", err.message);
-    return;
-  }
-  console.log("DB connected!");
-});
+//db.connect((err) => {
+//  if (err) {
+//    console.log("DB ERROR:", err.message);
+//    return;
+//  }
+//  console.log("DB connected!");
+//});
 
 // start page
 app.get("/", (req, res) => {
